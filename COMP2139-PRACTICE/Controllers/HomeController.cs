94f8174d4c53a1964a588d5821ba47a5c7f85d71
Sheikh Mohammad Farhan Rashidy <sheikh.rashidy02@gmail.com>
2025-03-08 15:31:47 -0500

    first commit

---
+++ COMP2139-PRACTICE/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using COMP2139_PRACTICE.Models;
+
+namespace COMP2139_PRACTICE.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ COMP2139-PRACTICE/Models/Project.cs
@@ -0,0 +1,6 @@
+namespace COMP2139_PRACTICE.Models;
+
+public class Project
+{
+    public int ProjectId { get; set; }
+}
