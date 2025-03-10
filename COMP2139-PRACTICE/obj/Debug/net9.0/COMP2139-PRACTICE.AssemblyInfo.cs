0467bdea69b926414019e024196515b8350998f3
Sheikh Mohammad Farhan Rashidy <sheikh.rashidy02@gmail.com>
2025-03-10 11:13:02 -0400

    migration

--- COMP2139-PRACTICE/obj/Debug/net9.0/COMP2139-PRACTICE.AssemblyInfo.cs
+++ COMP2139-PRACTICE/obj/Debug/net9.0/COMP2139-PRACTICE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("COMP2139-PRACTICE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0a69a4de1abaebca0f9f02a5a3c4b5274ae56b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bcf5b3f4bb1a212722be5eea89e36be073e176e")]
 [assembly: System.Reflection.AssemblyProductAttribute("COMP2139-PRACTICE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("COMP2139-PRACTICE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
